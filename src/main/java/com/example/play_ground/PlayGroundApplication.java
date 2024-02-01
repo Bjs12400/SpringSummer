e681ea1e351927eea9816b23c90ecd70b89cd14d
Bjs12400 <bjh12400@gmail.com>
2024-02-01 15:25:38 +0900

    commit

---
+++ src/main/java/com/example/play_ground/PlayGroundApplication.java
@@ -0,0 +1,14 @@
+package com.example.play_ground;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayGroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayGroundApplication.class, args);
+    }
+
+}
